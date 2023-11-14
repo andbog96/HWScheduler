7acf41869bd8794770479581d1c44735f9cb2589
pertsevpv <pertsevpv@yandex.ru>
2023-11-14 11:29:46 +0300

    Initial commit

---
+++ Android/app/src/main/java/com/example/hwscheduler/authorize/AuthorizeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.hwscheduler.authorize
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hwscheduler.R
+
+class AuthorizeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authorize)
+    }
+}
