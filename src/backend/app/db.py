4e02b67ea7abee3a12839268b070e63b7b891efe
sivalien <sivalien@icloud.com>
2023-11-14 18:15:00 +0300

    adde cursor

--- src/backend/app/db.py
+++ src/backend/app/db.py
@@ -8,6 +8,9 @@ conn = psycopg2.connect(
     host="localhost"
 )
 
+cur = conn.cursor()
+
+
 def create_user(login, password):
     cur.execute("INSERT INTO Users (login, password) VALUES (%s, %s) RETURNING userId", (login, password))
     user_id = cur.fetchone()[0]
